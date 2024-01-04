5c22ccd204d174c64118e34956c495004c405247
satriachandrapamungkas <satriachandrapamungkas@gmail.com>
2024-01-04 16:06:44 +0700

    skelethon fe lowongan pekerjaan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../src/assets/styles/index.css";
+import "../src/assets/styles/tailwind.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "../src/Auth/Landing";
+import Index from "./Auth/Index";
+import Login from "./Auth/auth/Login";
+import Register from "./Auth/auth/Register";
+import Profile from "./Auth/Profile";
+import Dashboard from "./Auth/admin/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dash" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
